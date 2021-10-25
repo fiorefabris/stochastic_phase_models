177d4ef143c451bc752ae7c3b7951c93d55dc87e
fiorefabris <40428686+fiorefabris@users.noreply.github.com>
2021-10-25 15:03:12 -0300

    Delete pulse_detection directory

--- pulse_detection/__init__.py
+++
@@ -1,5 +0,0 @@
-from pulse_detection.quantifiers_main import  compute_fft, compute_FPT,get_consecutive
-from pulse_detection.pulse_detection_main import compute_pulse_detection,compute_pulses_quantifiers
-
-
-
