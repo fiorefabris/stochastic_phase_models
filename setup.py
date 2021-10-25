80d3b60e6e6a56ce750ff4ea1758ac08c3260e60
fiorefabris <40428686+fiorefabris@users.noreply.github.com>
2021-10-25 15:17:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ setup(
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
-   packages=['adler'],  #same as name
-   install_requires=['matplotlib', 'pandas','numpy'], #external packages as dependencies
    packages=['adler', 'adler.get_time_series','adler.plotting','adler.pulse_detection'],
+   install_requires=['matplotlib', 'pandas','numpy'], #external packages as dependencies
 )
 #multiprocessing,,'itertools','functools','pickle','math','time'
 #scripts
