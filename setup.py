725fbb47006161528ce7433e6bfc443d39fc9af2
fiorefabris <40428686+fiorefabris@users.noreply.github.com>
2021-10-25 15:01:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(
+   name='adler',
+   version='1.0',
+   description='module for studying noisy adler statistics',
+   author='fiorefabris',
+   author_email='fiorefabris@gmail.com.com',
+   packages=['adler'],  #same as name
+   install_requires=['matplotlib', 'pandas', 'multiprocessing','numpy','itertools','functools','pickle','math','time'], #external packages as dependencies
+   scripts=[]
+)
