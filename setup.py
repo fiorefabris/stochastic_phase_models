92439118d8e6175a629cc5697f2b4c41830da911
fiorefabris <40428686+fiorefabris@users.noreply.github.com>
2021-10-25 16:11:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.12',
+   version='1.121',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
