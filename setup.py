1d722a622f5bd3bb398c4b0c601ab1f47b9dbc18
fiorefabris <fiorefabris27@gmail.com>
2021-11-01 14:07:25 -0300

    cambie el plotting para que haya una sola figura

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.130111',
+   version='1.130112',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
