b6adad46e02236c016f7508639df35574c388da7
fiorefabris <fiorefabris27@gmail.com>
2021-11-01 20:13:09 -0300

    cambie el EPS

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.1304',
+   version='1.1305',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
