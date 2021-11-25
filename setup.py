56f3645a04a8819fd9aa3ffb49a120ee84769808
fiorefabris <fiorefabris27@gmail.com>
2021-11-25 15:11:36 -0300

    bins nuevamente

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.147',
+   version='1.1471',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
