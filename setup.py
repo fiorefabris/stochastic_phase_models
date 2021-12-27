faa931003fe1629b028d974c0488f02163427622
Fiore <fiorefabris27@gmail.com>
2021-12-27 19:46:37 -0300

    errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.1478',
+   version='1.14781',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
