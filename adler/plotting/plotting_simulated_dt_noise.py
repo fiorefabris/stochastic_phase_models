664db1393e783e9b1ab22c067c582e478c42fe3a
Fiore <fiorefabris27@gmail.com>
2022-03-07 12:37:34 -0300

    plotting corrections con el all_combinantions 7

--- adler/plotting/plotting_simulated_dt_noise.py
+++ adler/plotting/plotting_simulated_dt_noise.py
@@ -36,15 +36,16 @@ def plot_theta_alpha(data_folder,save_path_name,params):
     
             ax.plot(initial_conditions,cond_prob,linewidth=1) #,color=colors[k]
             ax.plot(initial_conditions,cond_prob,'o', markersize = 2)
-            #ax.set_ylim([-1,110]); #ax.set_xlim([-1.05,1.05])
+            ax.set_ylim([-1,110]); ax.set_xlim([-np.pi/2,3/2*np.pi])
               
         if k == (D_*ALP - D_ ):
             ax.set_ylabel("ocurrences", fontsize=10);
             ax.set_xlabel("initial conditions", fontsize=10)
             ax.xaxis.set_label_coords(0.5, -0.1);
             ax.yaxis.set_label_coords(-0.05, 0.5)
-            set_scale(ax,[-np.pi/2,3/2*np.pi],[-0.5,110])
-            ax.set_xticklabels([-0.5,110]); ax.set_yticklabels([r'$-\frac{\pi}{2}',r'$\frac{3 \pi}{2}']); ax.tick_params(labelsize=10) 
+            #set_scale(ax,[-np.pi/2,3/2*np.pi],[-0.5,110])
+            ax.set_xticks([-np.pi/2,3/2*np.pi]);ax.set_yticks([0,100])
+            ax.set_yticklabels([0,100]); ax.set_xticklabels([r'$-\frac{\pi}{2}',r'$\frac{3 \pi}{2}']); ax.tick_params(labelsize=10) 
         else:
             silent_ax(ax)
             
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.20994',
+   version='1.20995',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
