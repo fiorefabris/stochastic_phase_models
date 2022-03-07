cc6f82d66ddf2ef4bd50145fa0a698c31227d7ea
Fiore <fiorefabris27@gmail.com>
2022-03-07 13:03:20 -0300

    new plot

--- adler/plotting/plotting_simulated_dt_noise.py
+++ adler/plotting/plotting_simulated_dt_noise.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from adler.data_managing_functions import download_data, check_file
 from adler.get_time_series.main import all_combinations
 from adler.plotting.plotting_main import set_scale,silent_ax
-
+import seaborn as sns
 #%%
     
 ###############################################################################
@@ -63,4 +63,56 @@ def plot_theta_alpha(data_folder,save_path_name,params):
     plt.savefig(save_path_name + 'plotting.pdf', format='pdf')
     return(0)
 
+#%%
+    
+###############################################################################
+###  plotting
+###############################################################################  
+    
+def plot_first_IV(data_folder,save_path_name,params):
+
+    
+    
+    colors =  sns.color_palette(sns.color_palette("viridis",len(params['alpha'])))
+
+    
+    fig, axs = plt.subplots(2,2, sharex=False, sharey=True, figsize=(8.27, 11.69))
+    fig.subplots_adjust(bottom=0.15, top=0.9, left=0.15, right=0.8, wspace=0.1, hspace=0.2)
+    ax = axs[0,0]; 
+  
+    omega = params['omega'][0]
+    
+    for k,alpha in enumerate(params['alpha']):
+        
+        result = []; D_aux = []
+       
+       for D in params['D']:                    
+                    print(alpha/omega,omega,D)
+                    cond_prob_filename = data_folder +  'cond_prob_omega_'+str(np.round(omega,3))+'_alpha_'+str(np.round(alpha/omega,3))+'_D_'+str(D)+'.pkl'
+                    #initial_conditions_filename = data_folder +  'initial_conditions_omega_'+str(np.round(omega,3))+'_alpha_'+str(np.round(alpha/omega,3))+'_D_'+str(D)+'.pkl'
+        
+                    if check_file(cond_prob_filename,""):
+                        cond_prob = download_data(cond_prob_filename)
+                        #initial_conditions = download_data(initial_conditions_filename)
+                        result.append(cond_prob[0])
+                        D_aux.append(D)
+                        
+        #ax.plot(initial_conditions,cond_prob,linewidth=1) #,color=colors[k]
+        ax.plot(D_,result,'o', markersize = 2,color = colors[k],label = str(alpha/omega))
+            
+        #ax.set_ylim([-1,110]); ax.set_xlim([-np.pi/2,3/2*np.pi])
+              
+        ax.set_ylabel("ocurrences", fontsize=10);
+        ax.set_xlabel("initial conditions", fontsize=10)
+        ax.xaxis.set_label_coords(0.5, -0.5);
+        ax.yaxis.set_label_coords(-0.4, 0.5)
+        ax.legend(fontsize=8, ncol=1, framealpha=0, fancybox=True)
+        #set_scale(ax,[-np.pi/2,3/2*np.pi],[-0.5,110])
+        #ax.set_xticks([-np.pi/2,3/2*np.pi]);ax.set_yticks([-1,110])
+        #ax.set_yticklabels([-1,110]); ax.set_xticklabels([r'$-\frac{\pi}{2}$',r'$\frac{3 \pi}{2}$']); ax.tick_params(labelsize=10) 
+        
+
+        
+    plt.savefig(save_path_name + 'plotting_FIC.pdf', format='pdf')
+    return(0)
 
