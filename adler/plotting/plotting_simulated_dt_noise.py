39c302737639f9a87163da36d0f9489e34addc78
Fiore <fiorefabris27@gmail.com>
2022-03-07 13:44:35 -0300

    new plot corrections 2

--- adler/plotting/plotting_simulated_dt_noise.py
+++ adler/plotting/plotting_simulated_dt_noise.py
@@ -73,7 +73,6 @@ def plot_first_IV(data_folder,save_path_name,params):
 
     
     
-    colors =  sns.color_palette(sns.color_palette("viridis",len(params['alpha'])))
 
     
     fig, axs = plt.subplots(2,2, sharex=False, sharey=True, figsize=(8.27, 11.69))
@@ -82,6 +81,10 @@ def plot_first_IV(data_folder,save_path_name,params):
   
     omega = params['omega'][0]
     
+########################## D plot #############################################  
+   
+    colors =  sns.color_palette(sns.color_palette("viridis",len(params['alpha'])))
+
     for k,alpha in enumerate(params['alpha']):
         
         result = []; D_aux = []
@@ -110,6 +113,39 @@ def plot_first_IV(data_folder,save_path_name,params):
         #set_scale(ax,[-np.pi/2,3/2*np.pi],[-0.5,110])
         #ax.set_xticks([-np.pi/2,3/2*np.pi]);ax.set_yticks([-1,110])
         #ax.set_yticklabels([-1,110]); ax.set_xticklabels([r'$-\frac{\pi}{2}$',r'$\frac{3 \pi}{2}$']); ax.tick_params(labelsize=10) 
+        
+    
+    
+######################## alpha plot ##########################################  
+    ax = axs[1,0]; 
+    colors =  sns.color_palette(sns.color_palette("viridis",len(params['D'])))
+
+    for k,D in enumerate(params['D']):
+        
+        result = []; ALP_aux = []
+       
+        for alpha in params['alpha']:                    
+            print(alpha/omega,omega,D)
+            cond_prob_filename = data_folder +  'cond_prob_omega_'+str(np.round(omega,3))+'_alpha_'+str(np.round(alpha/omega,3))+'_D_'+str(D)+'.pkl'
+        #initial_conditions_filename = data_folder +  'initial_conditions_omega_'+str(np.round(omega,3))+'_alpha_'+str(np.round(alpha/omega,3))+'_D_'+str(D)+'.pkl'
+        
+            if check_file(cond_prob_filename,""):
+                cond_prob = download_data(cond_prob_filename)
+                #initial_conditions = download_data(initial_conditions_filename)
+                result.append(cond_prob[0])
+                ALP_aux.append(D)
+                        
+        #ax.plot(initial_conditions,cond_prob,linewidth=1) #,color=colors[k]
+        ax.plot(ALP_aux,result,'o', markersize = 4,color = colors[k],label = str(D))
+            
+        #ax.set_ylim([-1,110]); ax.set_xlim([-np.pi/2,3/2*np.pi])
+              
+        ax.set_ylabel("ocurrences", fontsize=10);
+        ax.set_xlabel("alpha", fontsize=10)
+        ax.xaxis.set_label_coords(0.5, -0.1);
+        ax.yaxis.set_label_coords(-0.15, 0.5)
+        ax.legend(fontsize=8, ncol=1, framealpha=0, fancybox=True)
+
         
 
         
