0d596d7d51a8505eb8c03274bce0b3f7b6b9be4d
Fiore <fiorefabris27@gmail.com>
2022-03-07 12:17:43 -0300

    plotting corrections con el all_combinantions 3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.2098',
+   version='1.2099',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
