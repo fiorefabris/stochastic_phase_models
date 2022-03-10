0065fc5d9a595ec811c61cc50eeee372cbc93f47
Fiore <fiorefabris27@gmail.com>
2022-03-10 17:44:46 -0300

    plot nan values as nan

--- adler/plotting/plotting_simulated_dt_noise.py
+++ adler/plotting/plotting_simulated_dt_noise.py
@@ -13,7 +13,7 @@ def get_mean_value(list_):
         if len(i) > 0:
             aux.append(np.mean(i))
         else:
-            aux.append(0)
+            aux.append(np.nan)
     assert len(list_) == len(aux)
     return(aux)
 #%%
