24b2019a5b1c87471a28603282c1d619a39e97dd
fiorefabris <fiorefabris27@gmail.com>
2022-05-13 20:05:34 -0300

    cambios en importings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.22911',
+   version='1.22912',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
