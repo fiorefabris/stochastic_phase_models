18a2d55d5866367ec31624b66fa2c3bcc78829e1
fiorefabris <fiorefabris27@gmail.com>
2022-05-16 13:42:03 -0300

    cambios en plotting init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.2292',
+   version='1.22921',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
