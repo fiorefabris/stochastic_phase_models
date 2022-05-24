d8f7c1fde9ce655ecc4c7b32d194560e80dbc722
Fiore <fiorefabris27@gmail.com>
2022-05-24 15:42:40 -0300

    plotting t plus

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.229296',
+   version='1.229297',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
