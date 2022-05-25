5c2a17f35cb46264d149a4bec3dfae615be29cc8
fiorefabris <fiorefabris27@gmail.com>
2022-05-25 19:39:17 -0300

    pulse detection arguments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.2292992',
+   version='1.2292993',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
