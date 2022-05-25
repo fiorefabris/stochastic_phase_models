255f736ae3d96f5958d4146bd162c489cef62274
fiorefabris <fiorefabris27@gmail.com>
2022-05-25 20:35:32 -0300

    time series plotting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.2292997',
+   version='1.2292998',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
