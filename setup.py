f3725d09bbe4224e53e42f9891fa6f868e513461
Fiore <fiorefabris27@gmail.com>
2022-05-27 20:02:51 -0300

    delta key added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.229427',
+   version='1.229428',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
