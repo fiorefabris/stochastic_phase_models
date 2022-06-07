6d5c396cd350ec7005ec13ad9e903f5dcf9865a4
Fiore <fiorefabris27@gmail.com>
2022-06-07 18:07:29 -0300

    plotting ou time series

--- adler/plotting/plotting_time_series.py
+++ adler/plotting/plotting_time_series.py
@@ -293,3 +293,111 @@ def plot_time_series_square(dt,beg,T,d,N,Delta,description_file,data_folder,save
     
         plt.savefig(save_path_name + 'time_series_square_'+str(T0)+'.pdf', format='pdf')
     return(0)
+
+#%%
+def plot_time_series_square_ou(dt,beg,T,d,N,Delta,description_file,data_folder,save_path_name):
+    '''
+    plot_time_series_alpha(data_folder,save_path_name,dt,T,d,delta,tuple_)
+    Auxiliary funtion for parallelizong the time Series plotting function.
+    
+    Parameters
+    ----------
+    data_folder : string
+        path of where the time series are stored.  
+    save_path_name : string
+        path in where the first passage time statistics is going to be saved.
+    dt : float
+        time resolution of the time series. 
+    beg: principio de la TS. en tiempo
+    T : float
+        total time lenght of the time series. Is the interval you want to plot, starting from zero.
+    d : integer
+        the decimation factor of the experiment (i.e. how often do you save your time series in dt units).
+    N : int 
+        number of repetitions of the time series with same parameters.
+    Delta : integer
+        time series plotting resolution (i.e. how often do you want to plot your time series in datapoint units)
+
+    tuple : iterator
+        path of the datasheet with the time series parameters and saving names.
+
+
+    Returns
+    --------
+    Time series pdf files for one alpha value. 
+    In each column, there is one simulation corresponding to a D value. 
+    
+    See also
+    ---------
+    plot_time_series :  for auxiliary funtion for parallelizing the time Series plotting function.
+
+    '''
+
+######## Getting data information
+    ref = pd.read_excel(description_file,sheet_name='File_references')
+    ref.set_index('Unnamed: 0',inplace=True);
+    ###############################################################################
+    ### Plotting parameters
+    ###############################################################################    
+    xlim = [-5+beg,T+5] ; ylim = [-0.02,2.02] ;         
+    Cols = len(ref.groupby(['D'])) ;
+    for alpha,ref_ in ref.groupby(['alpha0']):
+        alpha in [i*2*np.pi/7 for i in [0,1,2]]:
+            
+            Rows = len(ref.groupby(['tau'])) ; 
+            Cols = len(ref.groupby(['sigma'])) ; 
+            colors =  sns.color_palette(sns.color_palette("viridis",Cols*1))
+            colors =  colors[::1]
+        ###############################################################################
+        ### Figure
+        ###############################################################################    
+        
+            fig, axs = plt.subplots(Rows, Cols, sharex=True, sharey=True, figsize=(8.27*5, 11.69*2))
+            fig.subplots_adjust(bottom=0.15, top=0.9, left=0.1, right=0.99, wspace=0.1, hspace=0.1)
+            
+            for col,(sigma,col_) in  enumerate(ref.groupby(['sigma'])):
+                for row, (tau,row_)  in  enumerate(col_.groupby(['tau'])):
+                    delta= np.round(alpha/col_.omega.unique()[0],4)  
+                    order = int(row_.order); number = int(row_.number)
+                    file_name =  str(number)+'_'+str(order)+'.pkl'
+                    ax = axs[row,col]; ax.grid(False);
+                    
+                    ################################################
+                    #### download data
+                    ################################################
+                    if check_file(file_name,data_folder):            
+                        
+                        theta = download_data(data_folder + file_name) 
+                        t = time(dt,T+beg,d)
+                        end = len(t)
+                        beg_ = int(beg/(dt*d))
+                        ax.plot(t[beg_:end:Delta],1+np.sin(theta)[beg_:end:Delta],linewidth=2,color=colors[col])
+                    
+                    ###############################################
+                    #### Plotting
+                    ################################################
+                    if row == 0:
+                        text = 'sigma = ' + str(np.round(sigma,5))
+                        ax.text(0.9, 1.05, text , ha='center', va='center', transform=ax.transAxes, fontsize=25)
+                    if col == 0:
+                        text = 'tau = ' + str(np.round(tau,5))
+                        ax.text(-0.2, 0.9, text , ha='center', va='center', transform=ax.transAxes, fontsize=25)
+        
+                    ax.set_ylim(ylim);
+                    ax.set_xlim(xlim)
+                    
+                    if (row == Rows-1) and (col == 0): 
+                        ax.set_ylabel(r'$1 + \sin(\theta)$', fontsize=30);
+                        ax.set_xlabel('time', fontsize=30)
+                        ax.xaxis.set_label_coords(0.5, -0.1);
+                        ax.yaxis.set_label_coords(-0.05, 0.5)
+                    
+                    set_scale(ax,[beg,T], [0,2])
+                    ax.set_xticklabels([beg,T])
+                    ax.set_yticklabels([0,2])
+                    ax.tick_params(labelsize=20)
+        
+        
+        
+            plt.savefig(save_path_name + 'time_series_square_ou_'+str(delta)+'.pdf', format='pdf')
+    return(0)
