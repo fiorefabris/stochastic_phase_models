4785d4431d16022885d40650e28994384351ff77
Fiore <fiorefabris27@gmail.com>
2022-06-14 12:42:18 -0300

    plotting activity

--- adler/plotting/plotting_consecutive.py
+++ adler/plotting/plotting_consecutive.py
@@ -6,7 +6,7 @@ from math import ceil
 import multiprocessing as mp
 from functools import partial 
 
-from adler.data_managing_functions import download_data,check_file,time
+from adler.data_managing_functions import download_data,check_file,time, load_activity
 from adler.plotting.plotting_main import set_scale,mask_arr
 
 
@@ -43,8 +43,33 @@ def load_consecutive_statistics(dataset,data_folder):
         return (mean_consecutive_value(consecutive_trains_dataset),total_pulses_dataset,isolated_pulses_dataset)
         
 #%%
-def plot_consecutiveness_(data_folder,save_folder,tuple_):
+        
+def plot_consecutiveness_activity(dt,beg,T,d,N,Delta,description_file,data_folder,save_folder):
+    '''
+    data folder: donde están los cc
+    '''
 
+    ref = pd.read_excel(description_file,sheet_name= 'File_references')
+    ref.set_index('Unnamed: 0',inplace=True);
+    pool = mp.Pool(processes= ceil(mp.cpu_count()))
+    
+    tuple_ = ref.groupby(['omega', 'alpha','D','number'])
+    plot_consecutiveness_activity__ = partial(plot_consecutiveness_activity_,dt,T,d,data_folder,save_folder)
+    pool.map(plot_consecutiveness_activity__,tuple_)
+    
+    plot_time_series_square_dataset_ = partial(plot_time_series_square_dataset,dt,beg,T,d,N,Delta,data_folder,save_folder)
+    pool.map(plot_time_series_square_dataset_,tuple_)
+    
+    pool.close()
+    pool.join()
+    return (2)
+
+def plot_consecutiveness_activity_(dt,T,d,data_folder,save_folder,tuple_):
+
+    
+# =============================================================================
+#     consecutiveness plot
+# =============================================================================
     (omega,alpha,D,number),dataset = tuple_[0],tuple_[1]
     delta = np.round(alpha/omega,4)  
     (mean_trains_cons,std_trains_cons),total_pulses,isolated_pulses = load_consecutive_statistics(dataset,data_folder)
@@ -53,25 +78,28 @@ def plot_consecutiveness_(data_folder,save_folder,tuple_):
     fig = plt.figure(constrained_layout=False, figsize=(8.27, 11.692))
     gs_main = gridspec.GridSpec(nrows=2, ncols=2, figure=fig); gs_main.update(left=0.1, right=0.9, bottom=0.1, top=0.90, hspace=0.3,wspace=0.3)
 
-    ax1 = plt.subplot(gs_main[1,0])
+    ax3 = plt.subplot(gs_main[1,0])
      
-    ax1.plot(np.arange(1,len(mean_trains_cons)+1),mean_trains_cons, linewidth=0.5, marker = "." , markersize=7, alpha=1)
-    ax1.fill_between(np.arange(1,len(mean_trains_cons)+1),mean_trains_cons-std_trains_cons,mean_trains_cons+std_trains_cons,alpha = 0.2)
+    ax3.plot(np.arange(1,len(mean_trains_cons)+1),mean_trains_cons, linewidth=0.5, marker = "." , markersize=7, alpha=1)
+    ax3.fill_between(np.arange(1,len(mean_trains_cons)+1),mean_trains_cons-std_trains_cons,mean_trains_cons+std_trains_cons,alpha = 0.2)
    
     #X_lim = [0,50]
-    #ax1.set_xlim(X_lim);
-    ax1.set_yscale('log')
-    #ax1.set_ylim(YC_lim)
-    ax1.set_ylabel('counts',fontsize=10); ax1.set_xlabel('length of sequence of \n consecutive pulses',fontsize=10)
-    ax1.xaxis.set_label_coords(0.5, -0.08);ax1.yaxis.set_label_coords(-0.2,0.5);
-    #ax1.set_xticks([0,3,6,9,12,15])        
+    #ax3.set_xlim(X_lim);
+    ax3.set_yscale('log')
+    #ax3.set_ylim(YC_lim)
+    ax3.set_ylabel('counts',fontsize=10); ax3.set_xlabel('length of sequence of \n consecutive pulses',fontsize=10)
+    ax3.xaxis.set_label_coords(0.5, -0.08);ax3.yaxis.set_label_coords(-0.2,0.5);
+    #ax3.set_xticks([0,3,6,9,12,15])        
+
+# =============================================================================
+#     consecutiveness boxplot
+# =============================================================================
     
-    
-    ax2 = plt.subplot(gs_main[1,1])
+    ax4 = plt.subplot(gs_main[1,1])
     arr = [total_pulses,isolated_pulses,[t-i for t,i in zip(total_pulses,isolated_pulses)]]
     
     X1 = [np.ones(len(arr[i]))*(i+1) for i in range(0,len(arr))]
-    bp1 = ax2.boxplot(arr,vert=True,whis=[5, 95],patch_artist=True,showmeans=False,meanline=True,showfliers=False )
+    bp1 = ax4.boxplot(arr,vert=True,whis=[5, 95],patch_artist=True,showmeans=False,meanline=True,showfliers=False )
 
     for i,box_ in enumerate(bp1['boxes']):
          box_.set( color=colors[i], linewidth=0.0,facecolor=colors[i],alpha = 0.1)# change outline color
@@ -86,38 +114,56 @@ def plot_consecutiveness_(data_folder,save_folder,tuple_):
     
     for i in range(len(X1)):
         xA = np.random.normal(0, 0.1, len(arr[i])), 
-        ax2.scatter(xA+X1[i],arr[i], alpha=1,s = 1.5,color='black',edgecolors='black',linewidths=0.0)
+        ax4.scatter(xA+X1[i],arr[i], alpha=1,s = 1.5,color='black',edgecolors='black',linewidths=0.0)
 
-    ax2.tick_params(axis='x', labelsize=8,length=2); 
-    ax2.tick_params(axis='y', labelsize=8,length=2)
-    ax2.set_xlabel('total,isolated,consecutive',fontsize=8)
-    ax2.set_ylabel('counts',fontsize=8)
-    #ax2.set_ylim([-1,200])
-    ax2.xaxis.set_label_coords(0.5, -0.12);ax2.yaxis.set_label_coords(-0.05,0.5)
+    ax4.tick_params(axis='x', labelsize=8,length=2); 
+    ax4.tick_params(axis='y', labelsize=8,length=2)
+    ax4.set_xlabel('total,isolated,consecutive',fontsize=8)
+    ax4.set_ylabel('counts',fontsize=8)
+    #ax4.set_ylim([-1,200])
+    ax4.xaxis.set_label_coords(0.5, -0.12);ax4.yaxis.set_label_coords(-0.05,0.5)
+    ax4.tick_params(labelsize=6,direction='out', pad=1,length=2)
+
+
+# =============================================================================
+#     activity population and mean plot
+# =============================================================================
+    ax2 = plt.subplot(gs_main[0,1]); plt.rc('axes.spines', top=False, bottom=True, left=True, right=False); 
+    
+    activity,silent,n_cell = load_activity(dataset,data_folder,dt,T,d)
+    
+    #population activity
+    if len(activity) > 0:
+        p1 = ax2.bar(np.arange(1 ,n_cell + 1),silent,width=1,color='darkgray',alpha=0.5,linewidth=0.0)
+        p2 = ax2.bar(np.arange(1 ,n_cell + 1),activity,bottom=silent,width=1,alpha=0.8,linewidth=0.0)
+        
+    ax2.set_xlim([0,n_cell]);ax2.set_ylim([0,100])
+    ax2.set_xlabel( ' trazas ',fontsize=8); 
+    ax2.set_xticks([1,n_cell + 1])
+    ax2.set_yticks([0,50,100])
     ax2.tick_params(labelsize=6,direction='out', pad=1,length=2)
-  
+    ax2.xaxis.set_label_coords(0.5,-0.06)
+    
+    #mean activity
+    ax1 = plt.subplot(gs_main[0,0]); plt.rc('axes.spines', top=False, bottom=True, left=True, right=False); 
+    
+    
+    if len(activity) > 0:
+        p1 = ax2.barh(np.arange(n_cell),width = np.mean(silent),xerr=np.std(silent),left =0,color='darkgray',alpha=0.5,linewidth=0.0,height=0.6)
+        p2 = ax2.barh(np.arange(n_cell),width = np.mean(activity),left=np.mean(silent),xerr = np.std(activity),color=colors[k],alpha=0.8,linewidth=0.0,height=0.6)
 
-    plt.savefig(save_folder+ 'consecutiveness_'+str(delta)+'_'+str(D)+'.pdf', format='pdf')
+    ax2.set_xticks([0,50,100])
+    ax2.set_ylim([0,100])
+    ax2.set_yticks([1,n_cell + 1])
+    ax2.tick_params(labelsize=6,direction='out', pad=1,length=2)
+    ax2.invert_yaxis()
     
-def plot_consecutiveness(dt,beg,T,d,N,Delta,description_file,data_folder,save_folder):
-    '''
-    data folder: donde están los cc
-    '''
+    
+    ax1.set_ylabel('fraction of cell track' ,fontsize=8); 
+    ax2.set_xlabel('fraction of cell track' ,fontsize=8); 
 
-    ref = pd.read_excel(description_file,sheet_name= 'File_references')
-    ref.set_index('Unnamed: 0',inplace=True);
-    pool = mp.Pool(processes= ceil(mp.cpu_count()))
+    plt.savefig(save_folder+ 'consecutiveness_activity_'+str(delta)+'_'+str(D)+'.pdf', format='pdf')
     
-    tuple_ = ref.groupby(['omega', 'alpha','D','number'])
-    plot_consecutiveness__ = partial(plot_consecutiveness_,data_folder,save_folder)
-    pool.map(plot_consecutiveness__,tuple_)
-    
-    plot_time_series_square_dataset_ = partial(plot_time_series_square_dataset,dt,beg,T,d,N,Delta,data_folder,save_folder)
-    pool.map(plot_time_series_square_dataset_,tuple_)
-    
-    pool.close()
-    pool.join()
-    return (2)
 
 
 def plot_time_series_square_dataset(dt,beg,T,d,N,Delta,data_folder,save_path_name,tuple_):
@@ -194,3 +240,8 @@ def plot_time_series_square_dataset(dt,beg,T,d,N,Delta,data_folder,save_path_nam
     
     plt.savefig(save_path_name + 'time_series'+str(delta)+'_'+str(D)+'.pdf', format='pdf')
     return(0)
+
+#%%
+    
+
+    
