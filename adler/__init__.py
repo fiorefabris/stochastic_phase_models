aebd740b6d3fb6beec158d8e0924ad40f52ed34b
Fiore <fiorefabris27@gmail.com>
2022-06-14 19:30:41 -0300

    dyncode_plotting

--- adler/__init__.py
+++ adler/__init__.py
@@ -1,3 +1,4 @@
 import adler.get_time_series 
 import adler.plotting 
 import adler.pulse_detection 
+adler.plotting.test_experimental_values
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.22953',
+   version='1.22954',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
