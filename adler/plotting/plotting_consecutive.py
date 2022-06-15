ef8295ab857431177eba3349754d1c9e0e93f187
Fiore <fiorefabris27@gmail.com>
2022-06-15 16:42:01 -0300

    plotting consecutive dist

--- adler/plotting/plotting_consecutive.py
+++ adler/plotting/plotting_consecutive.py
@@ -60,7 +60,7 @@ def load_consecutive_statistics(dataset,data_folder):
             consecutive_trains_dataset.append(consecutive_trial)
             total_pulses_dataset.append(consecutive_trial[0])
             
-            consecutive_pulses = [t-i for t,i in zip(consecutive_trial[0],isolated_pulses)]
+            consecutive_pulses = consecutive_trial[0]-isolated_pulses)
             consecutive_pulses_dataset.append(consecutive_pulses)
         
         return get_mean_value_place(consecutive_trains_dataset,True),total_pulses_dataset,isolated_pulses_dataset,consecutive_pulses_dataset
