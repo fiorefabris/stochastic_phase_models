fcc781989597d1ce3f4cfcfb28590757fe898afb
Fiore <fiorefabris27@gmail.com>
2022-07-14 20:37:52 -0300

    plotting 2d fit -> new pulse rate stattstics synthetic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.22967999999998',
+   version='1.22967999999999',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
