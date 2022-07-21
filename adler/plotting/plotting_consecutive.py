6c865b16aea2ee302299e1338e9504a85d727667
fiorefabris <fiorefabris27@gmail.com>
2022-07-21 13:36:50 -0300

    pulse rate hist

--- adler/plotting/plotting_consecutive.py
+++ adler/plotting/plotting_consecutive.py
@@ -199,7 +199,7 @@ def plot_consecutiveness_activity_(dt,T,d,data_folder,save_folder,dyncode_filena
     ax3 = plt.subplot(gs_row_1[2]) # aca va el histograma de pulse rate :)
     if len(DT) > 0:
         
-        bins = ax3.hist(pulse_rate,bins=100,density=True,alpha=1,linewidth=1); 
+        bins = ax3.hist(pulse_rate,bins=10,density=True,alpha=1,linewidth=1); 
         ax3.axvspan(0.0067, 0.02, color=green, alpha=0.3, lw=0)
         #tune_plot(ax,'dt (min)','probability density (1/min)',[0,20],1,[0,0.4],1,30,20)
         compute_st_values(ax3,pulse_rate,bins,1,10)   
