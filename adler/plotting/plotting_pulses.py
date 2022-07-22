fb73250287c8d15c51a2cf1c16950be89392756f
fiorefabris <fiorefabris27@gmail.com>
2022-07-21 22:04:41 -0300

    pulse rate hist

--- adler/plotting/plotting_pulses.py
+++ adler/plotting/plotting_pulses.py
@@ -749,7 +749,7 @@ def plot_2d_quantifiers(dt,T,d,description_file,data_folder,save_path_name):
     ref = pd.read_excel(description_file,sheet_name='File_references')
     ref.set_index('Unnamed: 0',inplace=True);
     
-    dt_quart = [6,8.33]; IPI_quart = [8,18.67];pulse_rate_quart = [0.0067, 0.02]#list(get_dyncode_pulse_rate_st(dyncode_file_name)) #mean, sigma
+    dt_quart = [6,8.33]; IPI_quart = [8,18.67];pulse_rate_quart =[0.02, 0.06] #list(get_dyncode_pulse_rate_st(dyncode_file_name)) #mean, sigma
     quartiles = [dt_quart,IPI_quart,IPI_quart,pulse_rate_quart] 
     
     vmM_dt = [1,10];vmM_IPI = [5,25]; vmM_pulse_rate = [0,0.16]
@@ -810,7 +810,8 @@ def plot_2d_superposition(dt,T,d,description_file,data_folder,save_path_name):
     ref = pd.read_excel(description_file,sheet_name='File_references')
     ref.set_index('Unnamed: 0',inplace=True);
     
-    dt_quart = [6,8.33]; IPI_quart = [8,18.67];pulse_rate_quart = [0.0067, 0.02]#list(get_dyncode_pulse_rate_st(dyncode_file_name)) #mean, sigma
+    dt_quart = [6,8.33]; IPI_quart = [8,18.67];pulse_rate_quart = [0.02, 0.06] #[0.0067, 0.02]
+    #list(get_dyncode_pulse_rate_st(dyncode_file_name)) #mean, sigma
     quartiles = [dt_quart,IPI_quart,pulse_rate_quart] 
     #name = ['dt','FPT','PulseRate']
     
