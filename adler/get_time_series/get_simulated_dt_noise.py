d258e35268b2795488429db54427cd5e0c172519
fiorefabris <fiorefabris27@gmail.com>
2022-08-31 15:17:17 -0300

    simulations...faster?

--- adler/get_time_series/get_simulated_dt_noise.py
+++ adler/get_time_series/get_simulated_dt_noise.py
@@ -127,7 +127,7 @@ def get_epsilon_plus_pop(main_filename,dt,T,p):
     PFE,PFI = get_fixed_points(alpha/omega)
     PFI = PFI - 2* np.pi
     #print(alpha/omega,PFI/np.pi*2,PFE/np.pi*2)
-    initial_conditions = get_geomspace(PFI,PFE,100)
+    initial_conditions = get_geomspace(PFI+1e-3,PFE,10)
     
     cond_prob = []
     steps_plus = []
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.229682212229956',
+   version='1.229682212229957',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
