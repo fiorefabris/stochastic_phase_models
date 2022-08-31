680f59acb4d2f3f66c5540470fef5303ec412536
fiorefabris <fiorefabris27@gmail.com>
2022-08-31 15:47:15 -0300

    un fft mas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.229682212229958',
+   version='1.229682212229959',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
