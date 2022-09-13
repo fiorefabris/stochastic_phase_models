c68f3913e663147c9cc31aee6b038906fe4fdfbe
fiorefabris <fiorefabris27@gmail.com>
2022-09-13 15:57:55 -0300

    plottinf omegap

--- adler/plotting/plotting_fft.py
+++ adler/plotting/plotting_fft.py
@@ -274,7 +274,7 @@ def plot_fft_alpha_all(save_path_name,data_folder,dt,d,tuple_):
     fig.subplots_adjust(bottom=0.15, top=0.9, left=0.15, right=0.8, wspace=0.1, hspace=0.2)
     #axs = axs.ravel();
     text = r'$\omega = \frac{2\pi}{7 min}$' + ' ~ ' + r'$ \alpha = $'+str(alpha) + r'$ \frac{2\pi}{7 min}$' 
-    axs[0,0].text(0.2,1.05, text, ha='center', va='center', transform=axs[0].transAxes, fontsize=12)
+    axs[0,0].text(0.2,1.05, text, ha='center', va='center', transform=axs[0,0].transAxes, fontsize=12)
 
     BETA = []; D_ = []; OMEGAP_=[]
     for k,(D,row) in enumerate(rows.groupby(['D'])):
