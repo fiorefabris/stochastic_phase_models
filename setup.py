00051afa0d14adff35181c93b0cce9ace1442407
fiorefabris <fiorefabris27@gmail.com>
2022-09-13 16:07:14 -0300

    plottinf omegap

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='adler',
-   version='1.229682212231211111',
+   version='1.2296822122312111111',
    description='module for studying noisy adler statistics',
    author='fiorefabris',
    author_email='fiorefabris@gmail.com.com',
